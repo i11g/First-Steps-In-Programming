e83a9527311336c6258bf53d3ed5241f9f07af3d
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-09-14 07:59:46 +0300

    New Tasks

---
+++ Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositAmount=double.Parse(Console.ReadLine());
+            int termsOfDeposit=int.Parse(Console.ReadLine());
+            double annualInterestRate=double.Parse(Console.ReadLine());
+
+            double finalAmount = depositAmount + termsOfDeposit * (depositAmount * annualInterestRate / 100) / 12;
+
+            Console.WriteLine(finalAmount);
+
+        }
+    }
+}
