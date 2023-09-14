c5e93c30d76cb3975e078ef6eebb30660c463f22
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-09-14 06:41:15 +0300

    Add project files.

---
+++ Console Converter USD BGN/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Console_Converter_USD_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd=double.Parse(Console.ReadLine());
+            double lev = usd * 1.79549;
+            Console.WriteLine(lev);
+        }
+    }
+}
